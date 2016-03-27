52e53e0c23f6e708dfa92d2b07986a839b9eb822
Oxyno-zeta <havrileckalexandre@gmail.com>
2016-03-27 17:39:20 +0200

    [Run] Logout auto

--- app/js/technical/run/splash.run.js
+++ app/js/technical/run/splash.run.js
@@ -11,7 +11,7 @@
 		.run(runLoading);
 
 	/** @ngInject */
-	function runLoading ($rootScope, $state, splashService) {
+	function runLoading ($rootScope, $state, splashService, systemService, popupService) {
 		$rootScope.$on('$stateChangeStart', function(event, _toState, _toParams) {
 			// Check if init is already done
 			if (!splashService.initialized){
@@ -22,6 +22,16 @@
 				event.preventDefault();
 
 				$state.go('splash');
+			}
+		});
+
+		$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+			// Check if it is an error from api
+			if (!_.isNull(error) && !_.isUndefined(error) && _.has(error, 'status') &&
+				(_.isEqual(error.status, 403) || _.isEqual(error.status, 401) || _.isEqual(error.status, 500))){
+				systemService.logout().finally(function(){
+					popupService.notAuthorized();
+				});
 			}
 		});
 	}
